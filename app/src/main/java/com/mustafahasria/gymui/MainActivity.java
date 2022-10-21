d3e98f50ab5d3896f99b666788f1b398dbd8a76e
Dell <melkonja00@gmail.com>
2022-10-21 12:26:36 +0300

    * This is the first interface

--- app/src/main/java/com/mustafahasria/gymui/MainActivity.java
+++ app/src/main/java/com/mustafahasria/gymui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mustafahasria.gymui;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
